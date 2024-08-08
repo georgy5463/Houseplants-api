5af9fa108ac51bb99fc5ace0552e49baf6f1adfe
georgy <georgytom5463@gmail.com>
2024-08-08 15:16:07 -0500

    configure user model and setup django admin

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ Django command to wait for the database to be available
 """
 import time
 from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import connect
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -17,6 +18,15 @@ class Command(BaseCommand):
         while not db_up:
             try:
                 self.check(databases=['default'])
+                # Attempt to create a connection to the database
+                connection = connect(
+                    dbname='devdb',
+                    user='devuser',
+                    password='changeme',
+                    host='db',
+                    port='5432'
+                )
+                connection.close()
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable. Waiting 1 sec...')
